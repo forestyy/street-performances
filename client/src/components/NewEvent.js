099087669c548a8b1984ed50b79cd5640215d9fb
foresty <foresty@Forests-MacBook-Pro.local>
2019-06-16 22:07:51 -0700

    form submission bug fix

--- client/src/components/NewEvent.js
+++ client/src/components/NewEvent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Menu, Dropdown, Form, Icon } from 'semantic-ui-react';
-import { DatePicker } from 'antd';
-
+import DatePicker from 'antd/lib/date-picker';
+import 'antd/lib/date-picker/style/css';
 
 class NewEvent extends Component {
 	constructor(props) {
@@ -9,7 +9,7 @@ class NewEvent extends Component {
 		this.state = {
 			artist: this.props.user.name,
 			date: null,
-			status: 'closed',
+			pickerIsOpen: false,
 			description: ''
 		}
 
@@ -38,14 +38,11 @@ class NewEvent extends Component {
 	}
 
 	handleDateChange = (date) => {
-		// if (date instanceof Date && !isNaN(date)) {
-		// 	this.setState({ date: date }, () => {console.log(this.state.date)});
-		// }
 		this.setState({ date: date }, () => {console.log(this.state.date)});
 	}
 
 	handleOpenChange = (status) => {
-		console.log(status);
+		this.setState({ pickerIsOpen: status }, () => {console.log(this.state.pickerIsOpen)});
 	}
 
 	handleDescriptionChange = (e, { value }) => {
@@ -68,8 +65,8 @@ class NewEvent extends Component {
 	}
 
 	render() {
-		const { artist, date, description } = this.state;
-		const isDone = artist && date && description;
+		const { artist, date, description, pickerIsOpen } = this.state;
+		const isDone = artist && date && description && !pickerIsOpen;
 		return (
 			<div>
 				<Menu size='massive' id='top-menu' secondary>
@@ -87,28 +84,21 @@ class NewEvent extends Component {
 				<Form size='huge' onSubmit={this.handleSubmit} id='eventForm'>
 					<h1>Event details</h1>
 					<Form.Input placeholder="Who's performing?" name='artist' value={artist} onChange={this.handleNameChange} />
-					{/*<DatePicker
-						id='event-date-picker'
-						required
-						fixedHeight
-						selected={date}
-						showTimeSelect
-						timeIntervals={15}
-						onChange={this.handleDateChange.bind(this)}
-						dateFormat="MM/dd/yyyy h:mm aa"
-						placeholderText="Date and time"
-					/>*/}
 					<Form.TextArea placeholder='Description' value={description} onChange={this.handleDescriptionChange}/>
 					<Form.Select
 					    placeholder='Category'
 					    fluid
 					    options={this.eventOptions}
 					/>
-					{/*<DayPicker
-			          selectedDays={this.state.date}
-			          onDayClick={this.handleDateChange}
-			        />*/}
-			        <DatePicker size="large" showTime={{ format: 'HH:mm' }} format="MM-DD-YYYY HH:mm" placeholder="Select Time" onOk={this.handleDateChange} />
+			        <DatePicker 
+			        	size="large" 
+			        	showTime={{ format: 'h:mm a' }} 
+			        	format="MM/DD/YYYY h:mm a" 
+			        	placeholder="Select Time" 
+			        	onOpenChange={this.handleOpenChange}
+			        	onChange={this.handleDateChange} 
+			        	onOk={this.handleDateChange} 
+			        />
 				</Form>
 			</div>
 		);
